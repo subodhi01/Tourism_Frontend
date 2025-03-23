61e58ac6bc3d0950f851c7fad294ae2b871e6c36
Subo <subodhinisanka@gmail.com>
2025-03-23 11:21:51 +0530

    subo3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,21 +3,12 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-<<<<<<< HEAD
-
 import { PlacesComponent } from '../app/components/places/places.component';
 import { UserProfileComponent } from './components/auth/userprofile/userprofile.component';
-
-
-=======
-import { PlacesComponent } from '../app/components/places/places.component';
-
-import { UserProfileComponent } from './components/auth/userprofile/userprofile.component';
-
-
-
 import { RequestBookingComponent } from './request-booking/request-booking.component';
->>>>>>> 22b85982f9f64228f0debd4a43a89187ccca472e
+
+
+
 
 export const routes: Routes = [
   { path: 'auth/places', component: PlacesComponent },
@@ -27,5 +18,5 @@ export const routes: Routes = [
   { path: 'auth/register', component: RegisterComponent },
   { path: 'auth/userprofile', component: UserProfileComponent },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' }, // Default route
-  { path: '**', redirectTo: 'auth/login' } // Wildcard route
+  { path: '', redirectTo: 'auth/login' } // Wildcard route
 ];
