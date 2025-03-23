7c62945f87011b98f022e6ca5579272f45be6f62
Subo <subodhinisanka@gmail.com>
2025-03-23 11:21:51 +0530

    subo3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { PlacesComponent } from '../app/components/places/places.component';
 
 import { UserProfileComponent } from './components/auth/userprofile/userprofile.component';
@@ -18,6 +19,14 @@ import { UserProfileComponent } from './components/auth/userprofile/userprofile.
 
 
 >>>>>>> d27bad1 (subo2)
+=======
+import { PlacesComponent } from '../app/components/places/places.component';
+import { UserProfileComponent } from './components/auth/userprofile/userprofile.component';
+import { RequestBookingComponent } from './request-booking/request-booking.component';
+
+
+
+>>>>>>> 61e58ac (subo3)
 
 export const routes: Routes = [
   { path: 'auth/places', component: PlacesComponent },
@@ -27,5 +36,5 @@ export const routes: Routes = [
   { path: 'auth/register', component: RegisterComponent },
   { path: 'auth/userprofile', component: UserProfileComponent },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' }, // Default route
-  { path: '**', redirectTo: 'auth/login' } // Wildcard route
+  { path: '', redirectTo: 'auth/login' } // Wildcard route
 ];
--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-<<<<<<< Updated upstream
 import { HomeComponent } from '../home/home.component';
-=======
-import { HomeComponent } from './home/home.component';
 import { UserProfileComponent } from './userprofile/userprofile.component';
 
->>>>>>> Stashed changes
+
 
 export const routes: Routes = [
   { path: 'auth/home', component: HomeComponent },
