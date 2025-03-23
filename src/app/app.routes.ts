d27bad1ea0f93d091a48ad3be28d9186d1380d42
Subo <subodhinisanka@gmail.com>
2025-03-23 11:11:47 +0530

    subo2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-<<<<<<< Updated upstream
+
 import { PlacesComponent } from '../app/components/places/places.component';
-=======
 import { UserProfileComponent } from './components/auth/userprofile/userprofile.component';
 
->>>>>>> Stashed changes
+
 
 export const routes: Routes = [
   { path: 'auth/places', component: PlacesComponent },
