e7b12fa236c65a34375689537796f2de4cf2bf52
Subo <subodhinisanka@gmail.com>
2025-03-25 21:36:54 +0530

    subo25.3.2

--- src/app/components/auth/shared/header/header.component.ts
+++ src/app/components/auth/shared/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +9,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  navigateToSignIn() {
+    this.router.navigate(['/auth/login']);
+  }
+
+  navigateToSignUp() {
+    this.router.navigate(['/auth/register']);
+  }
+}
